513c261dc8269119e3aca71c43bab181f3943a58
Amit Makasare <AMITMAKASARE>
2024-10-10 22:34:48 +0530

    Add animated hamburger menu

---
+++ main.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu");
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+  menu.classList.toggle("open-menu");
+  hamburgerMenu.classList.toggle("open");
+  console.log(hamburgerMenu);
+  
+}
