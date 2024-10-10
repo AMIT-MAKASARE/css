7e36e6f2c8da8cbcf0ccafa839bd4bb6bff2f642
AMIT-MAKASARE <amitmakasare747@gmail.com>
2024-10-10 17:17:45 +0000

    hamburger menu animation add

---
+++ Day2/main.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu");
+    const hamburgerMenu = document.querySelector(".hamburger-menu");
+  
+    menu.classList.toggle("open-menu");
+    hamburgerMenu.classList.toggle("open");
+    console.log(hamburgerMenu);
+    
+  }
+  
